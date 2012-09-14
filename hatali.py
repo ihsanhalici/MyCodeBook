aa0f7a73b589975723e17b5ffc09bb747df7babd
Halit <halit@laptop-Inspiron-N4050.(none)>
2012-09-14 19:21:27 +0300

    hatali

---
+++ hatali.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+import sys, string, random
+
+def random_capital_letter()
+  letters = string.uppercase
+      print "Capital Letter: "+random.choice(letters)
+          print ---
+
+          random_capital_letter()
